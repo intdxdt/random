52f991eb29ffbff8e6652740e7bfd362da51c495
Titus Tienaah <dxtitus@gmail.com>
2017-05-02 03:40:58 -0300

    random - first commit

---
+++ randstr.go
@@ -0,0 +1,21 @@
+package random
+
+import (
+	"time"
+	"math/rand"
+)
+
+const randChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func init() {
+    rand.Seed(time.Now().UnixNano())
+}
+
+func String(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = randChars[rand.Intn(len(randChars))]
+	}
+	return string(b)
+}
+
